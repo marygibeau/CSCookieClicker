420eb8b05e4aa15f3f331bc80fb7f54b76414672
Natalie Russell <nlrussell97@gmail.com>
2019-12-08 23:33:45 -0500

    Moved login out of render

--- src/App.js
+++ src/App.js
@@ -55,53 +55,6 @@ class App extends Component {
   }
 
   render() {
-    const Login = () => (
-      <div>
-        <div className="login">
-          <h2>Login</h2>
-          <form onSubmit={async event => {
-            event.preventDefault();
-            const name = event.target.username.value;
-            const pass = event.target.password.value;
-            if (await login({ name, pass })) {
-              console.log('logged in');
-              window.location.reload();
-            } else {
-              console.log('failed')
-            }
-
-          }}>
-            <div className="field">
-              <input id="username" className="input" placeholder="Username" type="text" name="username" />
-            </div>
-            <div className="field">
-              <input id="password" className="input" placeholder="Password" type="password" name="password" />
-            </div>
-            <input id="submitbutton" className="button is-primary" type="submit" value="Login" />
-          </form>
-        </div>
-
-
-        <div className="box has-background-white content">
-          <h2 className="has-text-dark">Create Account</h2>
-          <form onSubmit={async (e) => {
-            e.preventDefault();
-            const name = e.target.name.value;
-            const pass = e.target.pass.value;
-            await createAccount({ name, pass });
-          }}>
-            <div className="field">
-              <input className="input" placeholder="Username" type="text" name="name" />
-            </div>
-            <div className="field">
-              <input className="input" placeholder="Password" type="password" name="pass" />
-            </div>
-            <input className="button is-primary" type="submit" value={"Create"} />
-          </form>
-        </div>
-      </div>
-
-    );
 
     const About = () => (
       <div>
@@ -161,6 +114,54 @@ class App extends Component {
     );
   }
 }
+
+const Login = () => (
+  <div>
+    <div className="login">
+      <h2>Login</h2>
+      <form onSubmit={async event => {
+        event.preventDefault();
+        const name = event.target.username.value;
+        const pass = event.target.password.value;
+        if (await login({ name, pass })) {
+          console.log('logged in');
+          window.location.reload();
+        } else {
+          console.log('failed')
+        }
+
+      }}>
+        <div className="field">
+          <input id="username" className="input" placeholder="Username" type="text" name="username" />
+        </div>
+        <div className="field">
+          <input id="password" className="input" placeholder="Password" type="password" name="password" />
+        </div>
+        <input id="submitbutton" className="button is-primary" type="submit" value="Login" />
+      </form>
+    </div>
+
+
+    <div className="box has-background-white content">
+      <h2 className="has-text-dark">Create Account</h2>
+      <form onSubmit={async (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const pass = e.target.pass.value;
+        await createAccount({ name, pass });
+      }}>
+        <div className="field">
+          <input className="input" placeholder="Username" type="text" name="name" />
+        </div>
+        <div className="field">
+          <input className="input" placeholder="Password" type="password" name="pass" />
+        </div>
+        <input className="button is-primary" type="submit" value={"Create"} />
+      </form>
+    </div>
+  </div>
+
+);
 
 
 
