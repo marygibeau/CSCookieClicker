3a09146fb1395ca43d2f522f15c24b5721f9a8e0
Jeremiah Alfaro <jeremiahalfaro@gmail.com>
2019-12-12 15:39:45 -0500

    fixed updated post and get
    works now

--- src/Public.js
+++ src/Public.js
@@ -3,52 +3,48 @@ import { getAxiosInstance } from "./config/Axios";
 const axios = getAxiosInstance('/public');
 
 export async function postReview(name, rate) {
-
-    try {
-      console.log("posting review");
-      await axios.post('/reviews', { data: { "name": name, "rating": rate }});
-      return true;
-    } catch (error) {
-      return false;
-    }
+  let url = "/" + name;
+  try {
+    await axios.post(url, {
+      "name": name,
+      "data": {
+        "rating": rate
+      }
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 export async function getStatus() {
-    try {
-      return (await axios.get(`/reviews`)).data;
-    } catch (error) {
-      return false;
-    }
-  }
-
-
-export async function updateReview(name, rate) {
   try {
-      await axios.post(`/reviews`, {
-          "name": name,
-          "Authorization": "Bearer " + name,
-          "data": {
-              "rating": rate
-          }
-      });
-      return true;
+    let total = (await axios.get(`/`));
+    let sum = 0;
+    for(let i = 0; i < total.data.result.length; i++) {
+      let jer = await axios.get('/' + total.data.result[i])
+      sum += parseInt(jer.data.result.rating);
+    }
+    sum /= total.data.result.length;
+    console.log(sum);
+    return sum;
   } catch (error) {
-      return false;
+    return 0;
   }
 }
 
 export async function deleteReview(name) {
   try {
-      await axios.delete(
-          "/reviews",
-          {
-              headers: {
-                  Authorization: "Bearer " + name
-              }
-          }
-      );
-      return true;
+    await axios.delete("/reviews",
+      {
+        headers: {
+          Authorization: "Bearer " + name
+        }
+      }
+    );
+    return true;
   } catch (error) {
-      return false;
+    return false;
   }
 }
